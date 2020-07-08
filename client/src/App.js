5784c1a870fd10d654b87ea61477eadb650f21dc
Eula <eulabengco@gmail.com>
2020-07-07 22:10:43 -0400

    changed contacts array to a state

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,51 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import ListContacts from "./ListContacts";
 import avatar from "./icons/Mohan-muruge.jpg";
 
-const contacts = [
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "karen_isgrigg",
-    avatarURL: avatar,
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "richardkalehoff",
-    avatarURL: avatar,
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "tylermcginnis",
-    avatarURL: avatar,
-  },
-];
-=======
-import React, { Component } from 'react';
->>>>>>> master
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: avatar,
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: avatar,
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: avatar,
+      },
+    ],
+  };
+
+  removeContact = (contact) => {
+    //Way 1: pass in an object, every key of the object will be merged in the current state.
+    // this.setState({
+    //   key:"tyler"
+    // })
+
+    //Way 2: functional setState - passing an object and returning an object. callback setState so use this. use when updating satte component based on previous state, passes current state of the component
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id;
+      }),
+    }));
+  };
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <ListContacts contacts={contacts} />
-=======
-        Hello World
->>>>>>> master
+        <ListContacts
+          onDeleteContact={this.removeContact}
+          contacts={this.state.contacts}
+        />
       </div>
     );
   }
--- client/src/ListContacts.js
+++ client/src/ListContacts.js
@@ -1,29 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ListContacts extends Component {
-  render() {
-    return (
-      <>
-        <ol className="contact-list">
-          {this.props.contacts.map((contact) => {
-            return (
-              <>
-                <li key={contact.id} className="contact-list-item">
-                  <img src={contact.avatarURL} className="contact-avatar" />
-                  <div className="contact-details">
-                    <p>{contact.name}</p>
-                    <p>{contact.handle}</p>
-                    <button className="contact-remove">Remove</button>
-                  </div>
-                </li>
-                ;
-              </>
-            );
-          })}
-        </ol>
-      </>
-    );
-  }
+function ListContacts(props) {
+  return (
+    <>
+      <ol className="contact-list">
+        {props.contacts.map((contact) => {
+          return (
+            <>
+              <li key={contact.id} className="contact-list-item">
+                <img src={contact.avatarURL} className="contact-avatar" />
+                <div className="contact-details">
+                  <p>{contact.name}</p>
+                  <p>{contact.handle}</p>
+                </div>
+                <button
+                  onClick={() => props.onDeleteContact(contact)}
+                  className="contact-remove"
+                >
+                  Remove
+                </button>
+              </li>
+            </>
+          );
+        })}
+      </ol>
+    </>
+  );
 }
 
 export default ListContacts;
