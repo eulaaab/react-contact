2ab473a6cb26fa22fb07e52964b31406b6ec437c
Eula <eulabengco@gmail.com>
2020-07-23 10:47:43 -0400

    added component lifecycle methods, add contact component, push history

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,22 @@
 import React, { Component } from "react";
-import ListContacts from "./ListContacts";
+import ListContacts from "./ListContacts.js";
 import avatar from "./icons/Mohan-muruge.jpg";
+import * as ContactsAPI from "./utils/ContactsAPI.js";
+import CreateContact from "./CreateContact";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: "karen",
-        name: "Karen Isgrigg",
-        handle: "karen_isgrigg",
-        avatarURL: avatar,
-      },
-      {
-        id: "richard",
-        name: "Richard Kalehoff",
-        handle: "richardkalehoff",
-        avatarURL: avatar,
-      },
-      {
-        id: "tyler",
-        name: "Tyler McGinnis",
-        handle: "tylermcginnis",
-        avatarURL: avatar,
-      },
-    ],
+    contacts: [],
   };
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState(() => ({
+        contacts,
+      }));
+    });
+  }
 
   removeContact = (contact) => {
     //Way 1: pass in an object, every key of the object will be merged in the current state.
@@ -38,13 +30,40 @@ class App extends Component {
         return c.id !== contact.id;
       }),
     }));
+    ContactsAPI.remove(contact);
   };
+
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then(() => {
+      this.setState((currentState) => ({
+        contacts: currentState.contacts.concat([contact]),
+      }));
+    });
+  };
+  //pass a props to ListCOntacts, when it's clicked it'll toggle the screen.
   render() {
     return (
       <div>
-        <ListContacts
-          onDeleteContact={this.removeContact}
-          contacts={this.state.contacts}
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListContacts
+              onDeleteContact={this.removeContact}
+              contacts={this.state.contacts}
+            />
+          )}
+        />
+        <Route
+          path="/create"
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={(contact) => {
+                this.createContact(contact);
+                history.push("/");
+              }}
+            />
+          )}
         />
       </div>
     );
--- client/src/ListContacts.js
+++ client/src/ListContacts.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class ListContacts extends Component {
   static propTypes = {
@@ -40,8 +41,10 @@ class ListContacts extends Component {
               value={query}
               onChange={(event) => this.updateQuery(event.target.value)}
             />
+            <Link to="/create" className="add-contact">
+              Add Contact
+            </Link>
           </div>
-
           {showingContacts.length !== contacts.length && (
             <div className="showing-contacts">
               <span> Now Showing {showingContacts.length}</span>of{" "}
